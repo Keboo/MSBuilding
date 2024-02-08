f96a228600805ce7cf34e0482de0cfbb361359eb
Benjamin Michaelis <github@relay.benjamin.michaelis.net>
2024-02-07 20:34:53 -0800

    docs: Add comment to Build.ps1 (#1)

--- NuGet/Build.ps1
+++ NuGet/Build.ps1
@@ -1,3 +1,4 @@
+# You many need to make sure nuget.exe is installed first - https://learn.microsoft.com/nuget/install-nuget-client-tools?tabs=windows#nugetexe-cli
 nuget.exe pack CustomSdk -Verbosity detailed
 # Remove from the NuGet cache to ensure all builds get the latest
 Remove-Item "$env:USERPROFILE\.nuget\packages\customsdk" -ErrorAction Ignore -Recurse
